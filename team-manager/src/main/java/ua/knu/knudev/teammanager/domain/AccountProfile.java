9e5f558cb37a8053699a636937f668cf5cafb457
Denys <113017276+DenysLnk@users.noreply.github.com>
2024-10-28 13:17:59 +0200

    [KNUDEV-5] added account-department-specialty domain model (#13)
    * [KNUDEV-5] added some fields in account_profile entity and created department and Specialty entities
    
    * [KNUDEV-5] improved some bugs and added some columns
    
    * [KNUDEV-5] improved some bugs and added some additional tables
    
    * [KNUDEV-5] code cleaned
    
    * [KNUDEV-5] added constrains to all one-to-one and main-to-main tables
    
    * [KNUDEV-5] fixed some bugs
    
    * [KNUDEV-5] Finished domain
    
    ---------
    
    Co-authored-by: Vlad Shtaiier <shtaiier228666@gmail.com>

--- team-manager/src/main/java/ua/knu/knudev/teammanager/domain/AccountProfile.java
+++ team-manager/src/main/java/ua/knu/knudev/teammanager/domain/AccountProfile.java
@@ -2,14 +2,18 @@ package ua.knu.knudev.teammanager.domain;
 
 import jakarta.persistence.*;
 import lombok.*;
+import org.springframework.data.annotation.CreatedDate;
+import ua.knu.knudev.knudevsecurityapi.constant.AccountRole;
 
-@Entity
-@Table(schema = "team_management", name = "account_profile")
+import java.time.LocalDateTime;
+
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 @Builder
+@Entity
+@Table(schema = "team_management", name = "account_profile")
 public class AccountProfile {
 
     @Id
@@ -24,4 +28,30 @@ public class AccountProfile {
 
     @Column(nullable = false)
     private String middleName;
+
+    //    todo add validation by @knu.ua
+    @Column(nullable = false, updatable = false, unique = true)
+    private String email;
+
+    @Column
+    private String avatar;
+
+    @Column(nullable = false)
+    @Enumerated(EnumType.STRING)
+    private AccountRole accountRole;
+
+    @Column(nullable = false, updatable = false)
+    @CreatedDate
+    private LocalDateTime registrationDate;
+
+    @Column
+    private LocalDateTime lastRoleUpdateDate;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "department_id", referencedColumnName = "id", nullable = false)
+    private Department department;
+
+    @ManyToOne(cascade = CascadeType.PERSIST)
+    @JoinColumn(name = "specialty_code_name", referencedColumnName = "code_name", nullable = false)
+    private Specialty specialty;
 }
---
+++ team-manager/src/main/java/ua/knu/knudev/teammanager/domain/Specialty.java
@@ -0,0 +1,30 @@
+package ua.knu.knudev.teammanager.domain;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.HashSet;
+import java.util.Set;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(schema = "team_management", name = "specialty")
+public class Specialty {
+
+    @Id
+    @Column(name = "code_name", nullable = false, updatable = false)
+    private Double codeName;
+
+    @Column(nullable = false, updatable = false, unique = true)
+    private String name;
+
+    @ManyToMany(mappedBy = "specialties", fetch = FetchType.LAZY, cascade = CascadeType.PERSIST)
+    private Set<Department> departments = new HashSet<>();
+
+}
