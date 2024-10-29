11fea36ca77e7e0c7417d17a1c360fb97624d42d
DenysLeonenko <ds.leonenko@gmail.com>
2024-10-29 12:27:40 +0200

    [KNUDEV-6] some minnor changes

--- task-manager/src/test/java/ua/knu/knudev/query/dsl/QueryDslTest.java
+++ task-manager/src/test/java/ua/knu/knudev/query/dsl/QueryDslTest.java
@@ -1,137 +1,69 @@
-package ua.knu.knudev.query.dsl;
-
-import com.querydsl.jpa.JPQLQueryFactory;
-import com.querydsl.jpa.JPQLTemplates;
-import com.querydsl.jpa.impl.JPAQueryFactory;
-import jakarta.persistence.EntityManager;
-import jakarta.persistence.EntityManagerFactory;
-import org.easymock.EasyMock;
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
-import ua.knu.knudev.taskmanager.domain.QTask;
-
-import java.util.HashMap;
-import java.util.Map;
-import java.util.function.Supplier;
-
-import static org.junit.jupiter.api.Assertions.assertNotNull;
-
-public class QueryDslTest {
-
-    private EntityManagerFactory factoryMock;
-
-    private EntityManager mock;
-
-    private JPAQueryFactory queryFactory;
-
-    private JPQLQueryFactory queryFactory2;
-
-    private JPAQueryFactory queryFactory3;
-
-    private Map<String, Object> properties = new HashMap<>();
-
-    @BeforeEach
-    public void setUp() {
-        factoryMock = EasyMock.createMock(EntityManagerFactory.class);
-        mock = EasyMock.createMock(EntityManager.class);
-        Supplier<EntityManager> provider = () -> mock;
-        queryFactory = new JPAQueryFactory(JPQLTemplates.DEFAULT, provider);
-        queryFactory2 = queryFactory;
-
-        queryFactory3 = new JPAQueryFactory(provider);
-    }
-
-    @Test
-    public void queryNotNullTest() {
-        assertNotNull(queryFactory.query());
-    }
-
-    @Test
-    void query2Test() {
-        queryFactory2.query().from(QTask.task);
-    }
-
-    @Test
-    void query3Test() {
-        EasyMock.expect(mock.getEntityManagerFactory()).andReturn(factoryMock);
-        EasyMock.expect(factoryMock.getProperties()).andReturn(properties);
-        EasyMock.expect(mock.unwrap(EasyMock.anyObject(Class.class))).andReturn(mock).atLeastOnce();
-        EasyMock.replay(mock, factoryMock);
-
-        queryFactory3.query().from(QTask.task);
-        EasyMock.verify(mock, factoryMock);
-    }
-
-    @Test
-    public void fromFunctionTest() {
-        assertNotNull(queryFactory.from(QTask.task));
-    }
-
-    @Test
-    public void deleteFunctionTest1() {
-        assertNotNull(queryFactory.delete(QTask.task));
-    }
-
-    @Test
-    public void deleteFunctionTest2() {
-        queryFactory2.delete(QTask.task)
-                .where(QTask.task.name.length().gt(0));
-    }
-
-    @Test
-    public void deleteFunctionTest3() {
-        EasyMock.expect(mock.getEntityManagerFactory()).andReturn(factoryMock);
-        EasyMock.expect(factoryMock.getProperties()).andReturn(properties);
-        EasyMock.expect(mock.unwrap(EasyMock.anyObject(Class.class))).andReturn(mock).atLeastOnce();
-        EasyMock.replay(mock, factoryMock);
-
-        assertNotNull(queryFactory3.delete(QTask.task));
-        EasyMock.verify(mock, factoryMock);
-    }
-
-    @Test
-    public void updateFunctionTest1() {
-        assertNotNull(queryFactory.update(QTask.task));
-    }
-
-    @Test
-    public void updateFunctionTest2() {
-        queryFactory2.update(QTask.task)
-                .set(QTask.task.name, "Test name update")
-                .where(QTask.task.name.length().gt(0));
-    }
-
-    @Test
-    public void updateFunctionTest3() {
-        EasyMock.expect(mock.getEntityManagerFactory()).andReturn(factoryMock);
-        EasyMock.expect(factoryMock.getProperties()).andReturn(properties);
-        EasyMock.expect(mock.unwrap(EasyMock.anyObject(Class.class))).andReturn(mock).atLeastOnce();
-        EasyMock.replay(mock, factoryMock);
-
-        assertNotNull(queryFactory3.update(QTask.task));
-        EasyMock.verify(mock, factoryMock);
-    }
-
-    @Test
-    public void insertFunctionTest1() {
-        assertNotNull(queryFactory.insert(QTask.task));
-    }
-
-    @Test
-    public void insertFunctionTest2() {
-        queryFactory2.insert(QTask.task)
-                .set(QTask.task.name, "Test name insert");
-    }
-
-    @Test
-    public void insertFunctionTest3() {
-        EasyMock.expect(mock.getEntityManagerFactory()).andReturn(factoryMock);
-        EasyMock.expect(factoryMock.getProperties()).andReturn(properties);
-        EasyMock.expect(mock.unwrap(EasyMock.anyObject(Class.class))).andReturn(mock).atLeastOnce();
-        EasyMock.replay(mock, factoryMock);
-
-        assertNotNull(queryFactory3.insert(QTask.task));
-        EasyMock.verify(mock, factoryMock);
-    }
-
-}
+//package ua.knu.knudev.query.dsl;
+//
+//import com.querydsl.jpa.impl.JPAQueryFactory;
+//import org.junit.jupiter.api.BeforeEach;
+//import org.junit.jupiter.api.Test;
+//import org.junit.jupiter.api.extension.ExtendWith;
+//import org.mockito.Mock;
+//import org.mockito.Mockito;
+//import org.mockito.junit.jupiter.MockitoExtension;
+//import ua.knu.knudev.taskmanager.domain.QTask;
+//import ua.knu.knudev.taskmanager.domain.Task;
+//import ua.knu.knudev.taskmanager.repository.TaskRepository;
+//
+//import java.util.List;
+//import java.util.UUID;
+//
+//import static org.assertj.core.api.Assertions.assertThat;
+//
+//@ExtendWith(MockitoExtension.class)
+//public class QueryDslTest {
+//
+//    UUID task1Id = UUID.randomUUID();
+//    UUID task2Id = UUID.randomUUID();
+//    UUID task3Id = UUID.randomUUID();
+//    QTask qTask = QTask.task;
+//
+//    @Mock
+//    JPAQueryFactory jpaQueryFactory;
+//
+//    @Mock
+//    TaskRepository taskRepository;
+//
+//    @BeforeEach
+//    void enterTestDataInDB() {
+//        Task task1 = new Task();
+//        Task task2 = new Task();
+//        Task task3 = new Task();
+//
+//        task1.setId(task1Id);
+//        task1.setName("task1 Name");
+//        task1.setBody("task1 Body");
+//        task2.setId(task2Id);
+//        task2.setName("task2 Name");
+//        task2.setBody("task2 Body");
+//        task3.setId(task3Id);
+//        task3.setName("task3 Name");
+//        task3.setBody("task3 Body");
+//
+//        Mockito.when(taskRepository.save(task1)).thenReturn(task1);
+//        Mockito.when(taskRepository.save(task2)).thenReturn(task2);
+//        Mockito.when(taskRepository.save(task3)).thenReturn(task3);
+//    }
+//
+//    @Test
+//    void should_return_all_tasks_in_db() {
+//        List<Task> expectedTasks = List.of(
+//                new Task(task1Id, "task1 Name", "task1 Body"),
+//                new Task(task2Id, "task2 Name", "task2 Body"),
+//                new Task(task3Id, "task3 Name", "task3 Body")
+//        );
+//
+//        Mockito.when(jpaQueryFactory.selectFrom(qTask).fetch()).thenReturn(expectedTasks);
+//
+//        List<Task> allTasksFromDb = jpaQueryFactory.selectFrom(qTask).fetch();
+//
+//        assertThat(allTasksFromDb).hasSize(3);
+//        assertThat(allTasksFromDb).containsExactlyElementsOf(expectedTasks);
+//    }
+//}
