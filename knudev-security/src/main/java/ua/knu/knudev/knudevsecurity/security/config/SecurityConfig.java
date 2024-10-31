75a14784558acfab20aface7885b783ff66f4769
DenysLeonenko <ds.leonenko@gmail.com>
2024-10-31 11:23:05 +0200

    [KNUDEV-HOTFIX] added csrf disabled

--- knudev-security/src/main/java/ua/knu/knudev/knudevsecurity/security/config/SecurityConfig.java
+++ knudev-security/src/main/java/ua/knu/knudev/knudevsecurity/security/config/SecurityConfig.java
@@ -33,6 +33,7 @@ public class SecurityConfig {
     SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         http
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .csrf(AbstractHttpConfigurer::disable)
                 .authorizeHttpRequests(auth -> {
 //                    String user = AccountRole.USER.name();
 //                    String admin = AccountRole.ADMIN.name();
