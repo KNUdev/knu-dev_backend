eb51e5f70c832518d35f4e26f162fc31964539e2
DenysLeonenko <ds.leonenko@gmail.com>
2025-02-23 20:07:08 +0200

    [KNUDEV-96] some minor renaming

--- team-manager-api/src/main/java/ua/knu/knudev/teammanagerapi/dto/FullProjectDto.java
+++ team-manager-api/src/main/java/ua/knu/knudev/teammanagerapi/dto/FullProjectDto.java
@@ -24,7 +24,7 @@ public class FullProjectDto {
     @Schema(description = "Unique identifier of the project", example = "123e4567-e89b-12d3-a456-426614174000")
     private UUID id;
 
-    @Schema(description = "Project name (multi-language field)", example = "KNUdev")
+    @Schema(description = "Project name in english", example = "KNUdev")
     protected String name;
 
     @Schema(description = "Project description (multi-language field)", implementation = MultiLanguageFieldDto.class)
