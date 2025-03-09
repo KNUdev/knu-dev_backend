eb20e760dcea52058c98fb5230301c212b3be957
Vlad Shtaiier <shtaiier228666@gmail.com>
2025-03-09 14:19:51 +0200

    [KNUDEV-119] Fixed errors returns

---
+++ knudev-security-api/src/main/java/ua/knu/knudev/knudevsecurityapi/exception/LoginException.java
@@ -0,0 +1,9 @@
+package ua.knu.knudev.knudevsecurityapi.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+@RequiredArgsConstructor
+@Getter
+public class LoginException extends RuntimeException {
+}
